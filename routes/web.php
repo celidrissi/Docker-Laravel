a1655ca0826902c7d27457b1903981b0dac2b524
Chafik El idrissi <c.elidrissi@thirard.fr>
2021-03-04 17:47:26 +0100

    new Comit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
+    return 'Hello World';
+});
+
+Route::get('/{name}-{id}', function($name, $id){
+    return "Hello $name ;) Ton id est le $id ";
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
--- temp/php/resources/js/components/services/Service.js
+++
@@ -1,7 +0,0 @@
-export default class Service {
-
-    constructor(baseURL = "http:/localhost:8000/api") {
-        this.baseURL = baseURL
-    }
-
-}
